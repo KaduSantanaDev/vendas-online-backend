72270917fa8f96171ea3ce6042f986eb085544e9
KaduSantanaDev <kaduzera.ks@gmail.com>
2023-04-27 20:45:09 -0300

    feat: transform email in unique field

---
+++ src/migration/1682638511627-alter-table-user.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AlterTableUser1682638511627 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(
+            `alter table public.user add unique(email);`
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(``)
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { hash } from 'bcrypt';
 import { Repository } from 'typeorm';
@@ -14,6 +14,12 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const user = await this.findUserByEmail(createUserDto.email).catch(() => undefined)
+    
+    if (user) {
+      throw new BadRequestException('Email already registered in system')
+    }
+    
     const saltOrRounds = 10;
 
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
